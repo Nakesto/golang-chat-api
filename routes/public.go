c26076f03c773d432c5814327c53b26c7d443be8
Nakesto <vinboyhuang88@gmail.com>
2023-02-02 15:13:13 +0700

    deploy

--- routes/public.go
+++ routes/public.go
@@ -10,4 +10,7 @@ func Public(publicRoutes *gin.RouterGroup) {
 	publicRoutes.POST("/register", controller.Register)
 	publicRoutes.POST("/login", controller.Login)
 	publicRoutes.GET("/test", controller.Test)
+	publicRoutes.GET("/", func(c *gin.Context){
+	c.JSON(200, "Welcome to golang chat api")
+	})
 }
